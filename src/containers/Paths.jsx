0b56be737d17d199a638538c8e548f5c7853fa09
Hilary Osborne <h.t.osborne@gmail.com>
2018-03-19 11:57:50 +1000

    Added radium styleroot for media query support

--- src/containers/Paths.jsx
+++ src/containers/Paths.jsx
@@ -4,16 +4,20 @@ import { Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
 import { store, history } from 'scripts/store'
 import { Home, NotFound } from 'containers'
+import { StyleRoot } from 'radium'
 
 export class Paths extends React.Component {
   render () {
-    return (<Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </ConnectedRouter>
-    </Provider>)
+    return (
+      <StyleRoot>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route component={NotFound} />
+            </Switch>
+          </ConnectedRouter>
+        </Provider>
+      </StyleRoot>)
   }
 }
